04300054726d87f41d2beb9c7cd7ae714756fc49
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-06 06:19:36 -0400

    Added an error if the elements does not match the size of the array

--- integers_cube.py
+++ integers_cube.py
@@ -1,6 +1,14 @@
-#make a input for the size of array
+#for the size of array
+print("Cubes of integers")
+print("Hello User!\n")
 size = float(input("Enter the size of the array: "))
 
-#make a inpit for the integers of the array
+#for inputing the integers of the array
 elements = list(map(float, input("Enter the elements: ").split()))
+if len(elements) != size:
+    print(f"Invalid: You must enter exactly ({size}) elements. You entered ({len(elements)}) elements.")
+else:
 #display the cube of each integer
+    print("The cube of each elements: ")
+    for number in elements:
+        print(number ** 3)
