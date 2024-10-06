9dc08dd45f67e616d01a2521694cf2ff7dd53e44
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-06 05:49:42 -0400

    Added input for the elements on the list

--- integers_cube.py
+++ integers_cube.py
@@ -1,4 +1,6 @@
 #make a input for the size of array
-size = float(input("Enter the size of the array"))
+size = float(input("Enter the size of the array: "))
+
 #make a inpit for the integers of the array
+elements = list(map(float, input("Enter the elements: ").split()))
 #display the cube of each integer
